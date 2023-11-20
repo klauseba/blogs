eb86d16839af4d83a7846236749341bcd216b988
klauseba <66975395+klauseba@users.noreply.github.com>
2023-11-20 12:28:25 +0100

    Add files via upload

---
+++ src/NotFound.js
@@ -0,0 +1,10 @@
+const NotFound = () => {
+    return ( 
+        <div className="not-found"></div>
+        <h2>Sorry</h2>
+        <p>That page cannot be found</p>
+        <Link to="/">Back to the homepage...</Link>
+     );
+}
+ 
+export default NotFound; 
